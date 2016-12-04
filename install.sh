e501018ea2bf971517b0ddca50e492dbacb4f7bb
John Pradeep <yehohanan7@gmail.com>
2016-12-04 22:33:25 +0530

    fix installation location

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -o /usr/bin/remem https://media.githubusercontent.com/media/yehohanan7/remem/master/downloads/remem-0.1
-chmod +x /usr/bin/remem
+curl -o /usr/local/bin/remem https://media.githubusercontent.com/media/yehohanan7/remem/master/downloads/remem-0.1
+chmod +x /usr/local/bin/remem
 echo "remem s" >> ~/.profile
 echo "remem s" >> ~/.bashrc
 
