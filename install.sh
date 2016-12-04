9e614f1ff22d103115bd653de98fec2186d11b71
John Pradeep <yehohanan7@gmail.com>
2016-12-04 22:24:52 +0530

    update download link

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o /usr/bin/remem https://github.com/yehohanan7/remem/raw/master/downloads/remem-0.1
+curl -o /usr/bin/remem https://media.githubusercontent.com/media/yehohanan7/remem/master/downloads/remem-0.1
 echo "remem s" >> ~/.profile
 echo "remem s" >> ~/.bashrc
 
