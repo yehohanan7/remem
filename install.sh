42f2686e6911663a10af410624f6584bb2adef2c
John Pradeep <yehohanan7@gmail.com>
2016-12-04 22:48:52 +0530

    fix installations

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o /usr/local/bin/remem https://media.githubusercontent.com/media/yehohanan7/remem/master/downloads/remem-latest
+curl -o /usr/local/bin/remem https://raw.githubusercontent.com/yehohanan7/remem/master/downloads/remem-latest
 chmod +x /usr/local/bin/remem
 echo "remem s" >> ~/.profile
 echo "remem s" >> ~/.bashrc
