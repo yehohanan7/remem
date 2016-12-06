e3a4ee3a3e01396d19f6131b100e6663280a6dcd
John Pradeep <yehohanan7@gmail.com>
2016-12-06 12:14:56 +0530

    fix installations

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OS="`uname`"
-curl -o /usr/local/bin/remem https://raw.githubusercontent.com/yehohanan7/remem/master/downloads/remem-$OS
+curl -o /usr/local/bin/remem https://raw.githubusercontent.com/yehohanan7/remem/master/downloads/remem-${OS,,}
 chmod +x /usr/local/bin/remem
 echo "remem s" >> ~/.profile
 echo "remem s" >> ~/.bashrc
