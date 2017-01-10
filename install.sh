1bddc5fd8fbe07b1d88db0a3c0d29916379e4f20
John Pradeep <john@Johns-MacBook-Air.local>
2017-01-10 21:25:56 +0530

    fix typo

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 OS="`uname`"
 OS=$(echo $OS | tr '[A-Z]' '[a-z]')
 curl -o /usr/local/bin/remem https://raw.githubusercontent.com/yehohanan7/remem/master/downloads/remem-${OS}
-$chmod +x /usr/local/bin/remem
+chmod +x /usr/local/bin/remem
 echo "remem s" >> ~/.profile
 
