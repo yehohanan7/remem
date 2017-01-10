ae7b85d2d50f0af3785a2cb31bcb6cdde07be1aa
John Pradeep <john@Johns-MacBook-Air.local>
2017-01-10 21:23:18 +0530

    fix osx installation

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 OS="`uname`"
+OS=$(echo $OS | tr '[A-Z]' '[a-z]')
 curl -o /usr/local/bin/remem https://raw.githubusercontent.com/yehohanan7/remem/master/downloads/remem-${OS}
-chmod +x /usr/local/bin/remem
+$chmod +x /usr/local/bin/remem
 echo "remem s" >> ~/.profile
-echo "remem s" >> ~/.bashrc
 
